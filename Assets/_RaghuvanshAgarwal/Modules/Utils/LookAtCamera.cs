b97898aea5a5803a0106361e9ad4ea3c8b8e4a0c
Raghuvansh Agarwal <rarchva@gmail.com>
2025-09-30 17:06:52 +0530

    Look At Camera

--- Assets/_RaghuvanshAgarwal/Modules/Utils/LookAtCamera.cs
+++ Assets/_RaghuvanshAgarwal/Modules/Utils/LookAtCamera.cs
@@ -6,6 +6,8 @@ namespace _RaghuvanshAgarwal.Modules.Utils {
         private enum Mode {
             LookAt,
             LookAtInverted,
+            CameraForward,
+            CameraForwardInverted,
         }
         [SerializeField] private Mode mode;
         private void LateUpdate() {
@@ -16,6 +18,12 @@ namespace _RaghuvanshAgarwal.Modules.Utils {
                 case Mode.LookAtInverted:
                     Vector3 directionFromCamera = transform.position - Camera.main.transform.position;
                     transform.LookAt(transform.position + directionFromCamera);
+                    break;
+                case Mode.CameraForward:
+                    transform.forward = Camera.main.transform.forward;
+                    break;
+                case Mode.CameraForwardInverted:
+                    transform.forward = -Camera.main.transform.forward;
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
