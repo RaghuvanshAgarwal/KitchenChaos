20aefa226aec164d7d3161924317e05494317009
Raghuvansh Agarwal <rarchva@gmail.com>
2025-10-17 17:16:36 +0530

    Delivery Counter Visuals

---
+++ Assets/_RaghuvanshAgarwal/Modules/Counters/Delivery/Scripts/DeliveryCounter.cs
@@ -0,0 +1,16 @@
+using _RaghuvanshAgarwal.Modules.Counters.Scripts;
+using _RaghuvanshAgarwal.Modules.Kitchen_Objects.Plate.Scripts;
+using UnityEngine;
+
+namespace _RaghuvanshAgarwal.Modules.Counters.Delivery.Scripts {
+    public class DeliveryCounter : BaseCounter
+    {
+        public override void Interact(Player.Scripts.Player player) {
+            if (player.HasKitchenObject()) {
+                if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject)) {
+                    player.GetKitchenObject().DestroySelf();
+                }
+            }
+        }
+    }
+}
